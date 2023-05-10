d8fce233d23907bfeb27eba10312180d918435c1
Marc Gallagher <marcgallagher@gmail.com>
2023-05-10 10:33:30 -0700

    use submodules for vcpkg

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -2,18 +2,7 @@ include(FetchContent)
 
 option(USE_VCPKG "Use vcpkg to provide dependencies" ON)
 
-set(HOST_IS_WINDOWS $<STREQUAL:${CMAKE_HOST_SYSTEM_NAME},Windows>)
-set(VCPKG_EXE_NAME $<IF:${HOST_IS_WINDOWS},vcpkg.exe,vcpkg>)
-
 if(${USE_VCPKG})
-	FetchContent_Declare(
-		vcpkg
-		GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
-		GIT_TAG master
-		GIT_SHALLOW true
-		PATCH_COMMAND cmake -E rm -f ${VCPKG_EXE_NAME}
-	)
-
-	FetchContent_MakeAvailable(vcpkg)
-	include(${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake)
+	include(ext/vcpkg/scripts/buildsystems/vcpkg.cmake)
 endif()
+
