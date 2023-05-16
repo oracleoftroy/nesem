9cfdf31e66cda8a34c1875e0b2f49533fb779298
Marc Gallagher <marcgallagher@gmail.com>
2023-05-16 15:40:06 -0700

    use diagnostics:caret with msvc for better error messages

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -28,6 +28,7 @@ function(set_project_warnings project_name)
 		/w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
 		/permissive- # standards conformance mode for MSVC compiler.
 		/utf-8 # Set Source and Executable character sets to UTF-8
+		/diagnostics:caret
 	)
 
 	set(COMMON_WARNINGS
