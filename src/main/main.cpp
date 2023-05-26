a2a187660099a5cfbddd0b102432a2a9cbdd61fb
Marc Gallagher <marcgallagher@gmail.com>
2023-05-26 03:43:53 -0700

    emscripten build now passes nestest.nes in browser
    main can't keep running so data needs to be passed to the loop
    now dynamically allocating runtime data so it can live past main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,7 @@
 #include <filesystem>
 #include <fstream>
 #include <locale>
+#include <memory>
 #include <string_view>
 #include <vector>
 
@@ -14,12 +15,30 @@
 #	include <emscripten.h>
 #	include <emscripten/html5.h>
 
+struct RuntimeData
+{
+	RuntimeData(const std::string &title, cm::Sizei window_size, const app::Config &config)
+		: app{ui::App::create(title, window_size)}, core(app, config)
+	{
+	}
+
+	ui::App app;
+	app::NesApp core;
+};
+
 void run_once(void *user_data)
 {
-	auto *app = static_cast<ui::App *>(user_data);
+	auto data = std::unique_ptr<RuntimeData>{static_cast<RuntimeData *>(user_data)};
 
-	if (!app || !app->run_once())
+	if (!data || !data->app.run_once())
+	{
+		// loop finished retain ownership of the pointer so that it gets properly deleted
 		emscripten_cancel_main_loop();
+		return;
+	}
+
+	// loop continuing, release ownership of the pointer until next tick
+	data.release();
 }
 
 #endif
@@ -49,6 +68,12 @@ int application_main(int argc, char *argv[])
 
 	constexpr auto window_size = cm::Size{1024, 768};
 
+#if defined(__EMSCRIPTEN__)
+	auto runtime_data = std::make_unique<RuntimeData>("NES emulator", window_size, app::Config{});
+
+	LOG_INFO("starting wasm loop...");
+	emscripten_set_main_loop_arg(run_once, runtime_data.release(), 0, false);
+#else
 	auto app = ui::App::create("NES emulator", window_size);
 
 	if (!app)
@@ -57,10 +82,6 @@ int application_main(int argc, char *argv[])
 		return -1;
 	}
 
-#if defined(__EMSCRIPTEN__)
-	auto core = app::NesApp{app, app::Config{}};
-	emscripten_set_main_loop_arg(run_once, &app, 0, false);
-#else
 	auto config_path = ui::App::get_user_data_path("nesem") / "nesem.toml";
 	auto config = app::load_config_file(config_path);
 
@@ -69,8 +90,8 @@ int application_main(int argc, char *argv[])
 	app.run();
 
 	app::save_config_file(config_path, core.get_config());
+	LOG_INFO("Exiting...");
 #endif
 
-	LOG_INFO("Exiting...");
 	return 0;
 }
